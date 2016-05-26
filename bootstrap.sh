31f074ea894fd56021ee45d9ac5b98f1852ce4cd
somathor <somathor@gmail.com>
2016-05-26 13:59:18 +0900

    mod bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 sudo apt-get update
 sudo apt-get install -y git ansible 
 
-git clone https://github.com/jivoi/ansible-openvas.git
+git clone https://github.com/somathor/ansible-openvas.git
 cd  ansible-openvas
 ansible-playbook playbook.yml -i hosts --ask-sudo-pass
